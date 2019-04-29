089ad6b9c9f77b2bf17dbb599c7987b9472d1269
sam-peach <sampeachart@gmail.com>
2019-04-29 11:54:31 -0400

    update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+console.log("Hello from JavaScript");
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2FtLXBlYWNoIiwiYSI6ImNqdjJpcDRoYjI1c3A0M28wb2c3NW1iM2oifQ.qWTngVsoj6YlKdTri-Ygyg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
